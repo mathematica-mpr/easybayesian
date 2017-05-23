973e534c14e50a8f83dbb4702fead8be8a2c3821
rce-github <mdecamillis@mathematica-mpr.com>
2017-05-23 12:41:41 -0400

    Remove credible interval from posterior distribution

--- R/posteriorplot.R
+++ R/posteriorplot.R
@@ -2,13 +2,13 @@
 #' @export
 #' @import scales
 
-posteriorplot <- function(model, parameter, cutoff=0, 
+posteriorplot <- function(model, parameter, cutoff=0,
                           credibleIntervalWidth=0.95, lessthan=FALSE){
   posteriorSamples <- model$posteriorSamples$posteriorSamplesBeta
   whichParameter <- which(names(posteriorSamples)==parameter)
   posteriorDraws <- posteriorSamples[,whichParameter] # Input
   pointEstimate <- colMeans(posteriorSamples)
-  
+
   df.plot <- data.frame(density = posteriorDraws)
   if(lessthan){
     posteriorProbability <- apply(posteriorSamples, 2, function(x) return(mean(x<cutoff)))
@@ -24,22 +24,39 @@ posteriorplot <- function(model, parameter, cutoff=0,
   credibleInterval <- apply(posteriorSamples, 2, quantile,
                             c((1 - credibleIntervalWidth) / 2,
                               1 - (1 - credibleIntervalWidth) / 2))
-  p <- ggplot(df.plot, aes(density)) + geom_density() +
-    geom_vline(xintercept = cutoff, linetype = "dotdash") +
-    geom_area(data = ds_data,
-              aes(x = x, y = y),
-              alpha = 0.5,
-              fill = "darkgreen") +
-    custom_range(c(credibleInterval[1, whichParameter],
-                   credibleInterval[2, whichParameter]),
-                 pointEstimate[whichParameter]) +
-    annotate("text", x=mid, y=(density(df.plot$density, from=mid, to=mid, n=1)$y)/2,
-             label=prob, fontface="bold", family="sans") +
+  p <- ggplot(
+         df.plot,
+         aes(density)) +
+       geom_density() +
+       geom_vline(
+         xintercept = cutoff,
+         linetype = "dotdash") +
+       geom_area(
+         data = ds_data,
+         aes(
+           x = x,
+           y = y),
+         alpha = 0.5,
+         fill = "darkgreen") +
+      # Remove credible interval at bottom of plot - leaving here in case it becomes useful in the future
+      # custom_range(
+      #   c(credibleInterval[1, whichParameter],
+      #     credibleInterval[2, whichParameter]),
+      #     pointEstimate[whichParameter]) +
+       annotate("text",
+                x = mid,
+                y = (density(df.plot$density, from=mid, to=mid, n=1)$y) / 2,
+                label=prob,
+                fontface="bold",
+                family="sans") +
     theme_mpr() +
-    theme(axis.ticks = element_blank(), axis.text.y = element_blank()) +
-    xlab("Impact") + ylab("Posterior Probability")
+    theme(
+      axis.ticks = element_blank(),
+      axis.text.y = element_blank()) +
+    xlab("Impact") +
+    ylab("Posterior Probability Distribution")
   r <- ggplot_build(p)
-  
+
   p <- p +
     coord_cartesian(ylim = c(r$panel$ranges[[1]]$y.range[1]*1.05, r$panel$ranges[[1]]$y.range[2]))
 }
